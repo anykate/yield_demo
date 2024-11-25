6f939a001a2c17ac65ee1ce66940a346fefba965
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-11-25 08:31:12 +0530

    Initial Commit

---
+++ yield_demo.py
@@ -0,0 +1,13 @@
+def read_file(filename):
+    with open(filename, 'r') as f:
+        for line in f:
+            yield line.strip()
+
+
+def main():
+    for count, line in enumerate(read_file('sample_file.txt')):
+        print(f'{count}: {line}')
+
+
+if __name__ == '__main__':
+    main()
