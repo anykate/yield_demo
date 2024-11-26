f19d8b52723420e7dd5baf24c24b588fcaa8fe6f
Aniket Aryamane <aryamane.aniket@gmail.com>
2024-11-26 19:31:13 +0530

    Count time

--- yield_demo.py
+++ yield_demo.py
@@ -1,13 +1,21 @@
+import time
+
+
 def read_file(filename):
     with open(filename, 'r') as f:
         for line in f:
             yield line.strip()
 
 
-def main():
-    for count, line in enumerate(read_file('sample_file.txt')):
+def main(filename):
+    start = time.time()
+
+    for count, line in enumerate(read_file(filename)):
         print(f'{count + 1}: {line}')
+
+    end = time.time()
+    print(f"\nTotal time taken to read \"{filename}\": {end - start} seconds!")
 
 
 if __name__ == '__main__':
-    main()
+    main('sample_file.txt')
